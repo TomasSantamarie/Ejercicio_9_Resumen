cff04b53f7a754696ea858d615170b96b1ce2c0a
Tommy <tomsan@alu.centroafuera.es>
2023-01-27 09:35:54 +0100

    Commit (27/01/2023)

---
+++ app/src/main/java/com/example/ejercicio_9_resumen/CuartaPag.kt
@@ -0,0 +1,11 @@
+package com.example.ejercicio_9_resumen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CuartaPag : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cuarta_pag)
+    }
+}
